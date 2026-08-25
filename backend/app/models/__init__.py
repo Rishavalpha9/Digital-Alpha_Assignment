cf82c084c5fef9ecf70818181f18d27e73e16b6b
Rishavking1 <rishavkumarrajece@gmail.com>
2026-08-25 13:41:47 +0530

    initialize frontend and added dependencies file

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.redemption import Redemption
-from app.models.reward import Reward
-from app.models.transaction import Transaction
-from app.models.wallet import Wallet
-
-__all__ = ["Transaction", "Reward", "Wallet", "Redemption"]
